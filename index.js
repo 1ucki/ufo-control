5d87015cbf462d18dd495e50ace3dc179c76d36d
1ucki <fwillen@gmail.com>
2018-12-19 12:14:50 +0100

    changes for changes

---
+++ index.js
@@ -0,0 +1,58 @@
+const path = require('path')
+const express = require('express')
+const WebSocket = require('ws')
+const drone = require('ar-drone')
+
+const wss = new WebSocket.Server({ port: 3001 })
+
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.listen(port)
+
+const client = drone.createClient({ frameRate: 2 })
+const stream = client.getPngStream()
+
+let state = {
+  flying: false
+}
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    const msg = JSON.parse(data)
+    console.log(msg)
+
+    if (msg.type === 'command') {
+      if (msg.command === 'takeoff-land') {
+        if (state.flying) {
+          client.land()
+          state.flying = false
+        } else {
+          client.takeoff()
+          state.flying = true
+        }
+      } else if (msg.command === 'front') {
+        client.front(0.1)
+        setTimeout(() => {
+          client.stop()
+        }, 100)
+      } else if (msg.command === 'blink') {
+        client.animateLeds('blinkRed', 5, 1)
+      }
+    }
+  })
+  
+  stream.on('data', buffer => {
+    const msg = {
+      type: 'buffer',
+      buffer: buffer
+    }
+
+    if (ws.readyState === WebSocket.OPEN) {
+      ws.send(JSON.stringify(msg))
+    }
+  })
+})
+
+console.log(`drone streamin' at http://localhost:${ port }`)
---
+++ public/ufo.js
@@ -0,0 +1,46 @@
+const image = document.querySelector('#stream')
+
+const socket = new WebSocket('ws://localhost:3001')
+
+function stream(buffer) {
+  const int8Array = new Uint8Array(buffer.data)
+  const blob = new Blob([int8Array], { type: 'image/png' })
+  const url = URL.createObjectURL(blob)
+
+  image.src = url
+
+  setTimeout(() => {
+    URL.revokeObjectURL(url)
+  }, 500)
+}
+
+function sendCommand(command) {
+  const msg = {
+    type: 'command',
+    command: command
+  }
+
+  socket.send(JSON.stringify(msg))
+}
+
+socket.addEventListener('message', event => {
+  const msg = JSON.parse(event.data)
+
+  if (msg.type === 'buffer') {
+    stream(msg.buffer)
+  }
+})
+
+document.onkeydown = (event) => {
+  if (event.keyCode === 32) {
+    sendCommand('takeoff-land')
+  } else if (event.keyCode === 87) {
+    sendCommand('front')
+  } else if (event.keyCode === 83) {
+    sendCommand('back')
+  } else if (event.keyCode === 65) {
+    sendCommand('left')
+  } else if (event.keyCode === 68) {
+    sendCommand('right')
+  }
+}
